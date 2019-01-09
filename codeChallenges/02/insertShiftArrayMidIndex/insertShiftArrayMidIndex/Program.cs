a54ede3f60e8a1edf6c3f23ec7e649df6d0485ae
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-09 11:27:22 -0800

    fixed issues with Visual Studio

--- codeChallenges/02/insertShiftArrayMidIndex/insertShiftArrayMidIndex/Program.cs
+++ codeChallenges/02/insertShiftArrayMidIndex/insertShiftArrayMidIndex/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace insertShiftArrayMidIndex
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
