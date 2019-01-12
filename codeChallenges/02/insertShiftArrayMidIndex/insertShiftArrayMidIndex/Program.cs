faf51b4c5634eef8998603ca1d032d5d2d950ecf
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-12 15:36:26 -0800

    Finished Code Challenge 2.

--- codeChallenges/02/insertShiftArrayMidIndex/insertShiftArrayMidIndex/Program.cs
+++ codeChallenges/02/insertShiftArrayMidIndex/insertShiftArrayMidIndex/Program.cs
@@ -1,19 +1,43 @@
 ﻿using System;
 
-namespace array_shift
+namespace insertShiftArrayMidIndex
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            int[] inputArray = new int[] { 2, 4, 6, 8, 10, 11 };
-            int addValue = 25;
-            int[] outputArray = ArrayShift(inputArray, addValue);
-            for (int i = 0; i < outputArray.Length; i++)
+        { 
+            int[] inArray = new int[] { 2, 4, 6, 8, 10, 11 };
+            int addNum = 25;
+            //call in the parameters for external method
+            int[] outArray = ArrayShift(inArray, addNum);
+            for (int i = 0; i < outArray.Length; i++)
             {
-                Console.WriteLine(outputArray[i]);
+                Console.WriteLine(outArray[i]);
             }
         }
 
+        static int[] ArrayShift(int[] inArray, int addNum)
+        {
+            //set length of the new array
+            int[] outArray = new int[inArray.Length + 1];
+
+            //loop through the first half of the array
+            for (int i = 0; i < inArray.Length / 2; i++)
+            {
+                //assign indeces from input to output
+                outArray[i] = inArray[i];
+            }
+
+            //insert the additional value after the midpoint
+            outArray[inArray.Length / 2] = addNum ;
+
+            //loop through the second half of the array
+            for (int i = inArray.Length/2 + 1; i < outArray.Length; i++)
+            {
+                //assign indeces from input to output, after the midpoint
+                outArray[i] = inArray[i-1];
+            }
+            return outArray;
+        }
     }
 }
