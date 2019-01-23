aaadc1e2abc12bd5b81745dd49226ebc0b00fc93
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-23 11:15:53 -0800

    Queue Class complete

--- dataStructures/StacksAndQueues10/StacksAndQueues10/Classes/Queue.cs
+++ dataStructures/StacksAndQueues10/StacksAndQueues10/Classes/Queue.cs
@@ -4,7 +4,54 @@ using System.Text;
 
 namespace StacksAndQueues10.Classes
 {
-    class Queue
+    public class Queue
     {
+        /// <summary>
+        /// Sets Front Node
+        /// </summary>
+        public Node Front { get; set; }
+        /// <summary>
+        /// Sets Rear Node
+        /// </summary>
+        public Node Rear { get; set; }
+        /// <summary>
+        /// Sets Front & Rear Nodes To Queue
+        /// </summary>
+        /// <param name="node">Takes In The Node</param>
+        public Queue(Node node)
+        {
+            Front = node;
+            Rear = node;
+        }
+        /// <summary>
+        /// Enqueue Adds Node To The End & Sets Rear To The New Node
+        /// </summary>
+        /// <param name="value"></param>
+        public void Enqueue(int value)
+        {
+            Node node = new Node(value);
+            Rear.Next = node;
+            Rear = node;
+        }
+        /// <summary>
+        /// Dequeue removes node from front of queue
+        /// </summary>
+        /// <returns>a temp node</returns>
+        public Node Dequeue()
+        {
+            Node temp = Front;
+            Front = Front.Next;
+            temp.Next = null;
+                       
+            return temp;
+        }
+        /// <summary>
+        /// Peeks Is The Front Node
+        /// </summary>
+        /// <returns></returns>
+        public Node Peek()
+        {
+            return Front;
+        }
     }
 }
