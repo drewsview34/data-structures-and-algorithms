84c052782a042173b4a328b549848bc8ac89140a
Andrew Hinojosa <drewsview34@gmail.com>
2019-02-24 18:49:37 -0800

    Created console app and unit test. Completed read me.

---
+++ codeChallenges/RepeatedWord26/RepeatedWord26/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepeatedWord26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
