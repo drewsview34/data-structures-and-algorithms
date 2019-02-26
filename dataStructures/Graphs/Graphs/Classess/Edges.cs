bdfe79329d2f8ca39fadab6509381823d0c5b537
Andrew Hinojosa <drewsview34@gmail.com>
2019-02-25 17:42:54 -0800

    Created edges

---
+++ dataStructures/Graphs/Graphs/Classess/Edges.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graphs.Classess
+{
+    class Edges
+    {
+        public Vertex BeginA { get; set; }
+        public Vertex BeginB { get; set; }
+        public int Weight { get; set; }
+
+        public Edges(Vertex a, Vertex b, int weight)
+        {
+            BeginA = a;
+            BeginB = b;
+            Weight = weight;
+        }
+    }
+}
